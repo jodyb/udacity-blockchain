cae03369a0e0b03e1f1b355687f708f14be631d8
Jody Baty <jody@roamable.ca>
2018-10-18 20:24:54 -0500

    Added blockchain files from project 2

--- term1-project-three/api/controllers/BlockController.js
+++ term1-project-three/api/controllers/BlockController.js
@@ -4,9 +4,39 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
+ const Block = require("./Block");
+ const Blockchain = require("./Blockchain");
+ 
+ module.exports = {
+  /**
+   * `BlockController.create()`
+   */
+  create: async function (req, res) {
+      try {
+        //get description from req params
+        let {description} = req.allParams();
+        if (!description) {
+          return res.badRequest({err: 'block description is required'});
+        }
+        //create a new block
+        let newBlock = new Block(description);
 
-module.exports = {
-  
+        //add to blockchain
+
+        //return the new block in response
+        return res.ok('all good');
+      } catch (err) {
+        return res.serverError(err);
+      }
+  },
+
+  /**
+   * `BlockController.find()`
+   */
+  find: async function (req, res) {
+    return res.json({
+      todo: 'find() is not implemented yet!'
+    });
+  }
 
 };
-
