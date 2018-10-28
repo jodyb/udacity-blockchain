fdbaf72b2dc4803f2cc7169cbec029414a559489
Jody Baty <jody@roamable.ca>
2018-10-28 10:41:47 -0500

    Finished section - Web API post endpoint validates request with JSON response.

--- term1-project-four/api/controllers/ValidationController.js
+++ term1-project-four/api/controllers/ValidationController.js
@@ -5,29 +5,39 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+//setup global hash for requests
+global.walletAddressHash = global.walletAddressHash || {};
 module.exports = {
   /**
    * `ValidationController.requestValidation()`
    */
   requestValidation: async function (req, res) {
-
     try {
-      //get address from req params
+      //get address from req params and verify it is present
       let {address} = req.allParams();
-      //check that address is present
       if (!address) {
         return res.badRequest({err: 'Wallet address is required'});
       }
 
-      let requestTimeStamp = Math.round(new Date().getTime() / 1000);
+      //calculate validationWindow
+      let requestTimeStamp = global.walletAddressHash[address];
+      if (!requestTimeStamp) {
+        requestTimeStamp = Math.round(new Date().getTime() / 1000)
+      }
+      const currentTime =  Math.round(new Date().getTime() / 1000);
+      global.walletAddressHash[address] = requestTimeStamp;
+      const timeSinceFirstRequest = currentTime - requestTimeStamp;
+
+      //set up return message
       let message = `${address}:${requestTimeStamp}:starRegistry`;
       const validationWindow = 300;  //five minutes - 300 seconds
+      const timeRemaining = validationWindow - timeSinceFirstRequest;
 
       return res.json({
         "address": address,
         "requestTimeStamp": requestTimeStamp,
         "message": message,
-        "validationWindow": validationWindow
+        "validationWindow": timeRemaining
       });
     } catch (err) {
       return res.serverError(err);
