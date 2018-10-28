ffd3e2a5f8235717c1d6acc140c21af2535c126b
Jody Baty <jody@roamable.ca>
2018-10-27 21:16:18 -0500

    Response from request validation complete

--- term1-project-four/api/controllers/ValidationController.js
+++ term1-project-four/api/controllers/ValidationController.js
@@ -10,9 +10,27 @@ module.exports = {
    * `ValidationController.requestValidation()`
    */
   requestValidation: async function (req, res) {
-    return res.json({
-      todo: 'requestValidation() is not implemented yet!'
-    });
-  }
 
+    try {
+      //get address from req params
+      let {address} = req.allParams();
+      //check that address is present
+      if (!address) {
+        return res.badRequest({err: 'Wallet address is required'});
+      }
+
+      let requestTimeStamp = Math.round(new Date().getTime() / 1000);
+      let message = `${address}:${requestTimeStamp}:starRegistry`;
+      const validationWindow = 300;  //five minutes - 300 seconds
+
+      return res.json({
+        "address": address,
+        "requestTimeStamp": requestTimeStamp,
+        "message": message,
+        "validationWindow": validationWindow
+      });
+    } catch (err) {
+      return res.serverError(err);
+    }
+  }
 };
