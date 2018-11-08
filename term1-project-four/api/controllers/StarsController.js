7354e97992a4c52e6cdc53370e330c8dea35f9e3
Jody Baty <jody@roamable.ca>
2018-11-07 21:26:59 -0600

    Completed Step 3, Requirement 1: Search by Blockchain Wallet Address

--- term1-project-four/api/controllers/StarsController.js
+++ term1-project-four/api/controllers/StarsController.js
@@ -5,22 +5,20 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+const bitcoin = require('bitcoinjs-lib');
+const bitcoinMessage = require('bitcoinjs-message');
+const Block = require("./Block");
+const Blockchain = require("./Blockchain");
+global.blockchain = global.blockchain || new Blockchain();
+
 module.exports = {
   /**
    * `StarsController.searchByHash()`
    */
   searchByHash: async function (req, res) {
-    try {
-      //get address
-      let { hash } = req.allParams();
-
-      //check for valid required parameters
-      if (!hash) { return res.badRequest({err: 'address required'});};
-      console.log(hash);
-      return res.send(hash);
-    } catch (err) {
-      return res.serverError(err);
-    }
+    return res.json({
+      todo: 'searchByHash() is not implemented yet!'
+    });
   },
 
   /**
@@ -30,12 +28,18 @@ module.exports = {
     try {
       //get address
       let { address } = req.allParams();
-
       //check for valid required parameters
       if (!address) { return res.badRequest({err: 'address required'});};
-      console.log(address);
-      return res.send(address);
-
+      const height = await global.blockchain.getBlockHeight()
+      let starBlocks = [];
+      for(i = 0; i < height; i++) {
+        let block = await global.blockchain.getBlock(i);
+        if (block.body.address === address) {
+          block.body.star.storyDecoded = Buffer.from(block.body.star.story, 'hex').toString('ascii');
+          starBlocks.push(block);
+        }
+      }
+      return res.send(starBlocks);
     } catch (err) {
       return res.serverError(err);
     }
