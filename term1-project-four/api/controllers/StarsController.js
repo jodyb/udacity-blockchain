807738481ed256b85967e996a02c27fe3f0cf01a
Jody Baty <jody@roamable.ca>
2018-11-08 20:20:54 -0600

    Completed Requirement 2: Search by Star Block Hash

--- term1-project-four/api/controllers/StarsController.js
+++ term1-project-four/api/controllers/StarsController.js
@@ -14,15 +14,32 @@ global.blockchain = global.blockchain || new Blockchain();
 module.exports = {
   /**
    * `StarsController.searchByHash()`
+   *  Requirement 2: Search by Star Block Hash
    */
   searchByHash: async function (req, res) {
-    return res.json({
-      todo: 'searchByHash() is not implemented yet!'
-    });
+    try {
+      //get Star Block Hash
+      let { hash } = req.allParams();
+      //check for valid required parameters
+      if (!hash) { return res.badRequest({err: 'star block hash required'});};
+      const height = await global.blockchain.getBlockHeight();
+      let starBlocks = [];
+      for(i = 0; i < height; i++) {
+        let block = await global.blockchain.getBlock(i);
+        if (block.hash === hash) {
+          block.body.star.storyDecoded = Buffer.from(block.body.star.story, 'hex').toString('ascii');
+          starBlocks.push(block);
+        }
+      }
+      return res.send(starBlocks);
+    } catch (err) {
+      return res.serverError(err);
+    }
   },
 
   /**
    * `StarsController.searchByAddress()`
+   * Requirement 1: Search by Blockchain Wallet Address
    */
   searchByAddress: async function (req, res) {
     try {
