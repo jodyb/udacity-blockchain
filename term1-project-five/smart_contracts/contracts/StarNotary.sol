03a2b22c31f6cffcc6efcee7e1f32c3794cfa8f1
Jody Baty <jody@roamable.ca>
2018-11-18 17:04:46 -0600

    Completed Step 2, Part I, including update of tests

---
+++ term1-project-five/smart_contracts/contracts/StarNotary.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.23;
+
+import 'openzeppelin-solidity/contracts/token/ERC721/ERC721.sol';
+
+contract StarNotary is ERC721 {
+
+
+    struct Star {
+      string name;
+      string starStory;
+      string dec;
+      string mag;
+      string cent;
+    }
+
+    mapping(uint256 => Star) public tokenIdToStarInfo;
+    mapping(uint256 => uint256) public starsForSale;
+
+    function createStar(string _name, string _staryStory, string _dec, string _mag, string _cent, uint256 _tokenId) public {
+        Star memory newStar = Star(_name, _staryStory, _dec, _mag, _cent);
+        tokenIdToStarInfo[_tokenId] = newStar;
+        _mint(msg.sender, _tokenId);
+    }
+
+    function putStarUpForSale(uint256 _tokenId, uint256 _price) public {
+        require(this.ownerOf(_tokenId) == msg.sender);
+        starsForSale[_tokenId] = _price;
+    }
+
+    function buyStar(uint256 _tokenId) public payable {
+        require(starsForSale[_tokenId] > 0);
+
+        uint256 starCost = starsForSale[_tokenId];
+        address starOwner = this.ownerOf(_tokenId);
+        require(msg.value >= starCost);
+
+        _removeTokenFrom(starOwner, _tokenId);
+        _addTokenTo(msg.sender, _tokenId);
+
+        starOwner.transfer(starCost);
+
+        if(msg.value > starCost) {
+            msg.sender.transfer(msg.value - starCost);
+        }
+    }
+}
---
+++ term1-project-five/smart_contracts/test/StarNotaryTest.js
@@ -0,0 +1,60 @@
+const StarNotary = artifacts.require('StarNotary')
+
+contract('StarNotary', accounts => {
+
+    beforeEach(async function() {
+        this.contract = await StarNotary.new({from: accounts[0]})
+    })
+
+    describe('can create a star', () => {
+        it('can create a star and get its name', async function () {
+            await this.contract.createStar('Star power 103!', 'I love my wonderful star', 'ra_032.155', 'dec_121.874', 'mag_245.978', 1, {from: accounts[0]})
+            const star = await this.contract.tokenIdToStarInfo(1);
+            assert.equal(star[0], 'Star power 103!');
+            assert.equal(star[1], 'I love my wonderful star');
+            assert.equal(star[2], 'ra_032.155');
+            assert.equal(star[3], 'dec_121.874');
+            assert.equal(star[4], 'mag_245.978');
+        })
+    })
+
+
+    describe('buying and selling stars', () => {
+        let user1 = accounts[1]
+        let user2 = accounts[2]
+        let randomMaliciousUser = accounts[3]
+
+        let starId = 1
+        let starPrice = web3.toWei(.01, "ether")
+
+        beforeEach(async function () {
+          await this.contract.createStar('Star power 103!', 'I love my wonderful star', 'ra_032.155', 'dec_121.874', 'mag_245.978', 1, {from: accounts[1]});
+        })
+
+        it('user1 can put up their star for sale', async function () {
+            assert.equal(await this.contract.ownerOf(starId), user1)
+            await this.contract.putStarUpForSale(starId, starPrice, {from: user1})
+            assert.equal(await this.contract.starsForSale(starId), starPrice)
+        })
+
+        describe('user2 can buy a star that was put up for sale', () => {
+            beforeEach(async function () {
+                await this.contract.putStarUpForSale(starId, starPrice, {from: user1})
+            })
+
+            it('user2 is the owner of the star after they buy it', async function() {
+                await this.contract.buyStar(starId, {from: user2, value: starPrice, gasPrice: 0})
+                assert.equal(await this.contract.ownerOf(starId), user2)
+            })
+
+            it('user2 ether balance changed correctly', async function () {
+                let overpaidAmount = web3.toWei(.05, 'ether')
+                const balanceBeforeTransaction = web3.eth.getBalance(user2)
+                await this.contract.buyStar(starId, {from: user2, value: overpaidAmount, gasPrice: 0})
+                const balanceAfterTransaction = web3.eth.getBalance(user2)
+
+                assert.equal(balanceBeforeTransaction.sub(balanceAfterTransaction), starPrice)
+            })
+        })
+    })
+})
